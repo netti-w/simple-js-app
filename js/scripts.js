54e18049da42ebcc4ce8b0db125727e5c737e778
netti-w <106514885+netti-w@users.noreply.github.com>
2022-06-27 20:11:57 +0200

    Add pokemonList to start project

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
-
+pokemonList = [
+  {
+    name: 'Charmander',
+    height: 6,
+    types: [
+    'fire'
+    ],
+  },
+  {
+    name: 'Weedle',
+    height: 3,
+    types: [
+    'bug',
+    'poison'
+    ],
+  },
+  {
+    name: 'Parasect',
+    height: 10,
+    types: [
+    'grass',
+    'bug'
+    ],
+  },
+  {
+    name: 'Metapod',
+    height: 7, 
+    types: [
+    'bug'
+    ]
+  }
+];
