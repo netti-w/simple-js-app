e3ac5cc25b31bac2a12e5b571f64bf1ceb8fd599
netti-w <106514885+netti-w@users.noreply.github.com>
2022-07-05 15:09:20 +0200

    Change pokemonList to IIFE and update forEach loop to retrief pokemonList from IIFE functions
    - create pokemonRepository variable to hold what IIFE will return
    - wrap pokemonList array in an IIFE  to inssert in pokemonRepository variable
    - return an object with the public functions assigned as keys: getAll, add (IIFE returns only an object with the same names for keys as values)
    - update forEach loop using function add() returned by the IIFE in order to retrieve the pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,52 @@
-let pokemonList = [
-  {
-    name: 'Charmander',
-    height: 0.6,
-    types: [
-    'fire'
-    ],
-  },
-  {
-    name: 'Weedle',
-    height: 0.3,
-    types: [
-    'bug',
-    'poison'
-    ],
-  },
-  {
-    name: 'Parasect',
-    height: 1,
-    types: [
-    'grass',
-    'bug'
-    ],
-  },
-  {
-    name: 'Metapod',
-    height: 0.7,
-    types: [
-    'bug'
-    ]
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Charmander',
+      height: 0.6,
+      types: [
+      'fire'
+      ],
+    },
+    {
+      name: 'Weedle',
+      height: 0.3,
+      types: [
+      'bug',
+      'poison'
+      ],
+    },
+    {
+      name: 'Parasect',
+      height: 1,
+      types: [
+      'grass',
+      'bug'
+      ],
+    },
+    {
+      name: 'Metapod',
+      height: 0.7,
+      types: [
+      'bug'
+      ]
+    }
+  ];
+
+  function getAll() {
+      return pokemonList;
+    }
+  function add (item) {
+    return pokemonList.push(item);
   }
-];
+  return {
+    getAll: getAll,
+    add: add
+  }
+
+})();
 
 // Displaying all the pokemon names and height of all objects in PokemonList with an forEach loop
-pokemonList.forEach(function (pokemon) {
+pokemonRepository.getAll().forEach(function (pokemon) {
   if(pokemon.height > 0.7){
     document.write(`<p>"${pokemon.name}" (height: ${pokemon.height}) - Wow, that’s big!</p>`);
     /* Another way to write the output
