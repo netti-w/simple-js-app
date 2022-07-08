b5544b364a4dbce9e20e8399569010a42e531fdb
netti-w <106514885+netti-w@users.noreply.github.com>
2022-07-08 16:26:02 +0200

    Add Event llistener showing pokemon list items on click + changing button colour on click

--- js/scripts.js
+++ js/scripts.js
@@ -37,9 +37,27 @@ let pokemonRepository = (function () {
       return pokemonList;
     }
   function add (item) {
+    /*
+    // DOES NOT WORK!!!
+    if (typeof(item) !== 'object'){
+      console.log("Invalid entry! This is not an object");
+    }
+    else if (Object.keys(item) !== ['name', 'height', 'weight', 'types']) {
+      console.log("Invalid entry! Object keys name does not exist.");
+    }
+    else {
+      return pokemonList.push(item);
+    }*/
     return pokemonList.push(item);
   }
+  // Add function for next exercise (1.7)
+  function showDetails(pokemon) {
+    console.log(pokemon)
+  }
+
   function addListItem(pokemon) {
+    /* Creates a pokemon item as a button in the HTML list
+    that has pokemon-list class,  and assignes a button class*/
     let list = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
     let button = document.createElement('button');
@@ -47,6 +65,14 @@ let pokemonRepository = (function () {
     button.classList.add('button-class');
     listItem.appendChild(button);
     list.appendChild(listItem);
+
+    /* Event listener shows each pokemon item array in the console on click
+    and changes colour to green on click */
+    button.addEventListener('click', function (event) {
+      let target = event.target;
+      target.classList.toggle('button-class--green');
+      showDetails(pokemon);
+    });
   }
 
   return {
