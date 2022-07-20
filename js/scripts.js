66aa382f9ae86a9928f71b672bdce3fed25684bd
netti-w <106514885+netti-w@users.noreply.github.com>
2022-07-20 16:26:04 +0200

    Remove redundant, old addListitem and showModal object

--- js/scripts.js
+++ js/scripts.js
@@ -11,18 +11,6 @@ let pokemonRepository = (function () {
     pokemonList.push(pokemon);
   }
 
-  // function addListItem(pokemon) {
-  //   // Creates a pokemon item as a button in the HTML list that has pokemon-list class and assignes a button class
-  //   let list = document.querySelector('.pokemon-list');
-  //   let listItem = document.createElement('li');
-  //   let listButton = document.createElement('button');
-  //   listButton.innerText = pokemon.name;
-  //   listButton.classList.add('button-class');
-  //   listItem.appendChild(listButton);
-  //   list.appendChild(listItem);
-  //   listButtonEventListener(listButton, pokemon);
-  // }
-
   function addListItem(pokemon) {
     // Creates a pokemon item as a button in the HTML list that has pokemon-list class and assignes a button class
     let list = document.querySelector('.list-group');
@@ -38,38 +26,33 @@ let pokemonRepository = (function () {
     listButton.setAttribute('type', 'button');
     listButton.setAttribute('data-toggle', 'modal');
     listButton.setAttribute('data-target', '#pokemon-modal');
-    listButtonEventListener(listButton, pokemon);
+    listButtonEventListener(listButton, pokemon); // calling button event listener function
   }
 
-  // Event listener shows pokemon details in a modal on click
+  // Event listener open modal on click
   function listButtonEventListener(listButton, pokemon) {
     listButton.addEventListener('click', function (event) {
       showDetails(pokemon);
     });
   }
 
-  /* Promise fetching JSON data from external API url asynchronously
-  if response, then next promise fetching name and detailURL for each pokemon
-  and add to list via add function, if rejected show error in console */
   function loadList() {
+    // fetch method to asynchronously request pokemon data and return promise
     return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
+      return response.json(); // call response parsing data as JSON
+    }).then(function (json) { // return promise
+      json.results.forEach(function (item) { //successful -> call back actual data
         let pokemon = {
           name: item.name,
           detailsUrl: item.url
         };
         add(pokemon);
       });
-    }).catch(function (e) {
-      console.error(e);
+    }).catch(function (e) { // if rejected
+      console.error(e); // call back console error
     })
   }
 
-  /* Promise fetching data from detailURL,
-  if response next promise fetch specific details from detailURL, if data can't
-  load, show error */
   function loadDetails(pokemon) {
     let url = pokemon.detailsUrl;
     return fetch(url).then(function (response) {
@@ -86,7 +69,6 @@ let pokemonRepository = (function () {
     });
   }
 
-  // Promise fetching data asynchronously
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
       showModal(pokemon)
@@ -116,66 +98,6 @@ let pokemonRepository = (function () {
     modalBody.append(imageElement);
   }
 
-  // function showModal(pokemon) {
-  //   modalContainer.innerHTML = '';
-  //
-  //   let modal = document.createElement('div');
-  //   modal.classList.add('modal');
-  //
-  //   // Add the new modal content
-  //   let closeButtonElement = document.createElement('button');
-  //   closeButtonElement.classList.add('modal-close');
-  //   closeButtonElement.innerText = 'X';
-  //   closeButtonElement.addEventListener('click', hideModal);
-  //
-  //   let titleElement = document.createElement('h1');
-  //   titleElement.innerText = `${pokemon.name} (#${pokemon.id})`;
-  //
-  //   let contentElement = document.createElement('p');
-  //   contentElement.innerHTML = `Height: ${pokemon.height} cm
-  //   <br>Weight: ${pokemon.weight} g`;
-  //
-  //   let typesElement = document.createElement('p');
-  //   typesElement.innerText = 'Types: \n';
-  //
-  //   // Looping through types array to add all types in types details
-  //   pokemon.types.forEach(function(type) {
-  //     console.log(type.type.name);
-  //     typesElement.innerText += `${type.type.name}  \n`;
-  //   });
-  //
-  //   let imageElement = document.createElement('img');
-  //   imageElement.src = pokemon.imageUrl;
-  //
-  //   modal.appendChild(closeButtonElement);
-  //   modal.appendChild(titleElement);
-  //   modal.appendChild(imageElement);
-  //   modal.appendChild(contentElement);
-  //   modal.appendChild(typesElement);
-  //   modalContainer.appendChild(modal);
-  //   modalContainer.classList.add('is-visible');
-  // }
-  //
-  // function hideModal() {
-  //   modalContainer.classList.remove('is-visible');
-  // }
-  //
-  // // Adding eventListener closing modal if ESC key is pressed AND only if modal is open
-  // window.addEventListener('keydown', (e) => {
-  //   if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-  //     hideModal();
-  //   }
-  // });
-  //
-  // modalContainer.addEventListener('click', (e) => {
-  // // Since this is also triggered when clicking INSIDE the modal
-  // // We only want to close if the user clicks directly on the overlay
-  //   let target = e.target;
-  //   if (target === modalContainer) {
-  //     hideModal();
-  //   }
-  // });
-
   return {
     getAll: getAll,
     add: add,
@@ -187,7 +109,7 @@ let pokemonRepository = (function () {
 })();
 
 pokemonRepository.loadList().then(function() {
-  // Now the data is loaded!
+  // data is loaded!
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
